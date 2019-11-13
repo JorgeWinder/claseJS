ab558159672b8d783390b49a9266f035e8aea522
Jorge <jorge.winder@gmail.com>
2019-11-13 18:13:12 -0500

    Priemera clase JS

---
+++ Fundamentos/tipo-de-datos-string.js
@@ -0,0 +1,25 @@
+let cadena = " Claudia v d "
+
+console.log(cadena.trim())
+
+cadena = cadena.trim()
+
+// Propiedad //
+
+console.log("Canridad de caracteres de la cadena : " + cadena.length)
+
+// Metodos //
+
+console.log(cadena.indexOf("u"))
+console.log(cadena.replace("vd","Alejandra"))
+
+// cadena = 1500
+// console.log(cadena.toString())
+// console.log(parseInt(cadena))
+
+console.log(cadena.split(" "))
+console.log(cadena.substring(2,5))
+
+
+
+
---
+++ Fundamentos/tipo-de-datos.js
@@ -0,0 +1,29 @@
+let variable1 = 1 
+let variable2 = "String" 
+let variable3 = 14.5
+let variable4 = true
+let arreglo = [10,"texto", 58.5, false]
+
+const constante1 = 100
+const constante2 = "Claudia" 
+
+//-------------------------
+
+variable1 = 22
+variable4 = false
+
+// constante1 = 1000
+// constante2 = "Jorge"
+
+//-------------------------
+
+console.log(variable1)
+console.log(variable2)
+console.log(variable3)
+console.log(variable4)
+
+console.log(arreglo)
+
+console.log(constante1)
+console.log(constante2)
+
